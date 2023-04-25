252f42a1bc8b08033e6a959bafa9656c75e21019
felipe-cruz <piradous@gmail.com>
2023-04-25 17:04:56 -0300

    Feat: Req 01 - challenge_study_schedule.py

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+    students = 0
+    for time in permanence_period:
+        if not isinstance(time[0], int) or not isinstance(time[1], int):
+            return None
+        if time[0] <= target_time <= time[1]:
+            students += 1
+
+    return students
