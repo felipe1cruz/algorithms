6ca40033d194d9c4fba494e393b3dbc865c70a00
felipe-cruz <piradous@gmail.com>
2023-04-26 10:38:47 -0300

    tests: req 02, test test_encrypt.py

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,22 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    correct_message = "oi, tudo bem?"
+    even_key = 2
+    odd_key = 1
+    range_key = 20
+    wrong_message = 37
+    wrong_key = "oi"
+    assert encrypt_message(correct_message, even_key) == '?meb odut ,_io'
+
+    assert encrypt_message(correct_message, odd_key) == 'o_?meb odut ,i'
+
+    assert encrypt_message(correct_message, range_key) == '?meb odut ,io'
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message(correct_message, wrong_key)
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(wrong_message, even_key)
