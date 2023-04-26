a0424ee03e799a5a40fee52e0e6753a190622309
felipe-cruz <piradous@gmail.com>
2023-04-26 11:45:51 -0300

    feat- req 04, teste local não funcionou - challenge_anagrams.y

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,22 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    first_string_sorted = quick_sort(list(first_string.lower()))
+    second_string_sorted = quick_sort(list(second_string.lower()))
+
+    return first_string_sorted == second_string_sorted
+
+
+def quick_sort(string):
+    if len(string) <= 1:
+        return string
+
+    pivo = string[0]
+    menores = []
+    maiores = []
+
+    for letter in string[1:]:
+        if letter <= pivo:
+            menores.append(letter)
+        else:
+            maiores.append(letter)
+
+        return quick_sort(menores) + [pivo] + quick_sort(maiores)
